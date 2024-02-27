b6d996a0439784d868db49024174c3a771fdf733
root <root@LAPTOP-PI4MQAGH>
2024-02-27 20:39:03 +0100

    play_done

--- src/modules/puissance.js
+++ src/modules/puissance.js
@@ -7,68 +7,53 @@ export class Puissance_4 {
     this.player_1_color = options.player_1_color || "red";
     this.player_2_color = options.player_2_color || "yellow";
     this.board = [];
-    for (let i = this.rows; i >= 0; i--) {
-      let row = [];
-      for (let j = 0; j < this.cols; j++) {
-        row.push(j);
+    for (let i = 0; i < this.cols; i++) {
+      let column = [];
+      for (let j = 0; j < this.rows; j++) {
+        column.push(0);
       }
-      this.board.push(row);
+      this.board.push(column);
     }
-    console.log(this.board);
     this.current_player = this.player_1_id;
-    this.board;
     this.game_over = false;
     this.element = elt;
     this.render();
-    // this.set_pion();
   }
+
   render() {
     let table = document.createElement("table");
     for (let i = this.rows - 1; i >= 0; i--) {
-      // let row = [];
       let tr = table.appendChild(document.createElement("tr"));
       for (let j = 0; j < this.cols; j++) {
-        // row.push(j);
         let elem_td = document.createElement("td");
         elem_td.setAttribute("id", j + "-" + i);
         elem_td.className = "pion";
         let td = tr.appendChild(elem_td);
+
         td.addEventListener("click", (event) => {
           let cell = event.target.getAttribute("id").split("-");
           let x = parseInt(cell[0]);
           let y = parseInt(cell[1]);
-          console.log(x);
-          console.log(y);
-          this.board[x][y] = this.current_player;
-          let pion = event.target;
-          if (this.current_player === this.player_1_id) {
-            console.log(this.player_1_color);
-            pion.style.backgroundColor = this.player_1_color;
-            this.current_player = this.player_2_id;
-          } else {
-            pion.style.backgroundColor = this.player_2_color;
-            this.current_player = this.player_1_id;
+
+          for (let i = 0; i < this.rows; i++) {
+            if (this.board[x][i] === 0) {
+              this.board[x][i] = this.current_player;
+              let pion = document.getElementById(x + "-" + i);
+              if (this.current_player === this.player_1_id) {
+                console.log(this.player_1_color);
+                pion.style.backgroundColor = this.player_1_color;
+                this.current_player = this.player_2_id;
+              } else {
+                pion.style.backgroundColor = this.player_2_color;
+                this.current_player = this.player_1_id;
+              }
+              break;
+            }
           }
         });
       }
-      // this.board.push(row);
     }
     this.element.innerHTML = "";
     this.element.appendChild(table);
   }
-  // set_pion() {
-  //   console.log(this.board);
-  //   let cell = event.target.getAttribute("id").split("-");
-  //   let x = parseInt(cell[0]);
-  //   let y = parseInt(cell[1]);
-  //   this.board[x][y] = this.current_player;
-  //   let pion = event.target;
-  //   if (this.current_player === this.player_1_id) {
-  //     pion.style.backgroundColor = this.player_1_color;
-  //     this.current_player = this.player_2_id;
-  //   } else {
-  //     pion.style.backgroundColor = this.player_2_color;
-  //     this.current_player = this.player_1_id;
-  //   }
-  // }
 }
